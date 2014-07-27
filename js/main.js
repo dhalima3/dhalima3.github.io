a95f2bdd7d37d608a026386635a3a198dc50a0b3
Daryl Halima <dhalima@gatech.edu>
2014-07-27 16:38:29 -0400

    navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//SCROLLING STICKY HEADER
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > 200 ) {
+    $('header').addClass('navbar-fixed-top');
+    $('header').fadeIn();
+   } else if ( $(this).scrollTop() <= 200 ) {
+    $('header').fadeOut();
+  }
+});
