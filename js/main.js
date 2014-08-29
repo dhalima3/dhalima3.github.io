13dca6ac831c6682393d8eff00ff085a8ad3ea5c
dhalima3 <dhalima@gatech.edu>
2014-08-28 22:26:51 -0400

    Slider, etc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+//INITIALIZE FLEXSLIDER
+$(window).load(function(){
+	$('.flexslider').flexslider();
+});
+
+// INITIALIZE EASY PIE CHART
+$(function() {
+    $('.chart').easyPieChart({
+    	easing: 'easeOutBounce',    	
+    	barColor: "#3498db",
+    	scaleColor: false,
+    	size: 150, //need to change CSS too (.chart width and height), (.percent line-height)
+		onStep: function(from, to, percent) {
+			$(this.el).find('.percent').text(Math.round(percent));
+		}    	
+    });
+});
+
 //SCROLLING STICKY HEADER
 $(window).scroll(function () {
   if ( $(this).scrollTop() > 200 ) {
